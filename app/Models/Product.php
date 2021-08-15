973e1366319b359459c24d3e467b90fe461b1a15
Abel Palomino <abelpr87@gmail.com>
2021-08-15 01:31:14 -0500

    RestApi List Category and Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'price',
+        'stock',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
